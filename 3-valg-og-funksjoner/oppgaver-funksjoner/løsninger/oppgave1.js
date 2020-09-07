806602e4b46a6b99251e6da4ba72ddde59f29ee4
thorcc <thorchrcow@gmail.com>
2020-09-07 15:00:20 +0200

    uke3 valg og funksjoner

---
+++ 3-valg-og-funksjoner/oppgaver-funksjoner/løsninger/oppgave1.js
@@ -0,0 +1,56 @@
+// --- Eks ----
+
+console.log("---Eksempel---")
+
+function diameterSirkel(radius){
+    console.log(2 * Math.PI * radius);
+}
+
+diameterSirkel(3); // 18.84
+diameterSirkel(7); // 43.98
+
+
+// --- 1a ---
+console.log("--- 1a ---")
+/* 
+I denne oppgaven skal du fulføre funksjonen arealSirkel,
+slik at den logger arealet av sirkler med radius 24 og 32 i konsollen
+
+OBS! I denne oppgaven skal du KUN endre/legge til kode mellom krøllparentesene i funksjonen.
+Tips1: Formel for areal av sirkel er: Pi * r^2
+Tips2: Pi i javascript er Math.PI
+*/
+
+function arealSirkel(radius){
+    // Skriv koden din her.
+    console.log(radius * radius * Math.PI);
+}
+
+arealSirkel(24);
+arealSirkel(32);
+
+
+// --- 1b ---
+console.log("---1b---")
+/*
+I denne oppgaven skal du definere en funksjon med navn volumSirkel,
+som skal logge volumet av kuler med radius 11 og 21.
+
+OBS! Fjern skråstrekene før funksjonskallene for at funksjonen skal kjøre
+Tips: Volum av en kule er (4/3) * Pi * r^3
+*/
+
+// Skriv koden din her.
+
+function volumKule(radius){
+    console.log((4/3) * Math.pow(radius,3) * Math.PI);
+}
+
+volumKule(11);
+volumKule(21);
+
+
+// --- Fasit ---
+console.log("--- Fasit ---");
+console.log("1a: 1809.55 og 3216.99");
+console.log("1b: 5575.27 og 38792.38");
