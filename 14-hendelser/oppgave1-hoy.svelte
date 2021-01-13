d8375066e7d35c8d740207b2ed88c8bd8ff19562
thorcc <thorchrcow@gmail.com>
2021-01-13 12:55:17 +0100

    14-hendelser

---
+++ 14-hendelser/oppgave1-hoy.svelte
@@ -0,0 +1,26 @@
+<svelte:head>
+	<title>IT2 - Oppgave 1</title>
+</svelte:head>
+
+<h1>Oppgave 1 - Høy måloppnåelse</h1>
+
+<script>
+	let dyr = ["hest", "hund", "katt", "love"];
+	let lyd;
+
+	function trykk(art){
+		lyd.src = art + ".wav";
+		lyd.play();
+	}
+</script>
+
+{#each dyr as ettDyr}
+	<img on:click={() => trykk(ettDyr)} src="{ettDyr}.jpg" alt="">
+{/each}
+<audio bind:this={lyd} src="" controls></audio>
+	
+<style>
+	img{
+		width: 200px;
+	}
+</style>
---
+++ 14-hendelser/oppgave1-middels.svelte
@@ -0,0 +1,34 @@
+<h1>Oppgave 1 - middels måloppnåelse</h1>
+
+<script>
+	let hund;
+	let katt;
+	let hest;
+	let love;
+
+	function spillLyd(art){
+		if(art === "hund"){
+			hund.play();
+		}
+		else if(art === "katt"){
+			katt.play();
+		}
+		else if(art === "hest"){
+			hest.play();
+		}
+		else if(art === "love"){
+			love.play();
+		}
+	}
+
+</script>
+
+<img on:click={()=> spillLyd("hund")} src="hund.jpg" alt="">
+<img on:click={()=> spillLyd("katt")} src="katt.jpg" alt="">
+<img on:click={()=> spillLyd("hest")} src="hest.jpg" alt="">
+<img on:click={()=> spillLyd("love")} src="love.jpg" alt="">
+
+<audio bind:this={hund} src="hund.wav"></audio>
+<audio bind:this={katt} src="katt.wav"></audio>
+<audio bind:this={hest} src="hest.wav"></audio>
+<audio bind:this={love} src="love.wav"></audio>
